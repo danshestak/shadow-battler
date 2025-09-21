f5ca1cb72f2f3ab4137659ba89802567edbe34b9
Dan Shestak <187454689+danshestak@users.noreply.github.com>
2025-09-21 01:35:41 -0400

    spring initializr

---
+++ src/main/java/com/shadowbattlergroup/shadowbattler/ShadowbattlerApplication.java
@@ -0,0 +1,13 @@
+package com.shadowbattlergroup.shadowbattler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShadowbattlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShadowbattlerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shadowbattlergroup/shadowbattler/ShadowbattlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shadowbattlergroup.shadowbattler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShadowbattlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
