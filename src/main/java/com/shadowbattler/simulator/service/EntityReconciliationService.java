347fa2a779a13dee0ecf5edb3e89ca9af7077e88
Dan Shestak <187454689+danshestak@users.noreply.github.com>
2026-03-16 02:17:22 -0400

    constructor and start method signature for EntityReconciliationService

--- src/main/java/com/shadowbattler/simulator/service/EntityReconciliationService.java
+++ src/main/java/com/shadowbattler/simulator/service/EntityReconciliationService.java
@@ -2,36 +2,50 @@ package com.shadowbattler.simulator.service;
 
 import org.springframework.stereotype.Service;
 
-// import com.shadowbattler.simulator.persistence.service.MoveEntityService;
-// import com.shadowbattler.simulator.persistence.service.BattleResultEntityService;
-// import com.shadowbattler.simulator.persistence.service.OpponentEntityService;
-// import com.shadowbattler.simulator.persistence.service.SpeciesEntityService;
+import com.shadowbattler.simulator.persistence.service.MoveEntityService;
+import com.shadowbattler.simulator.persistence.service.BattleResultEntityService;
+import com.shadowbattler.simulator.persistence.service.OpponentEntityService;
+import com.shadowbattler.simulator.persistence.service.SpeciesEntityService;
 
 @Service
 public class EntityReconciliationService {
-    // private final BattleResultEntityService battleResultEntityService;
-    // private final SpeciesEntityService speciesEntityService;
-    // private final OpponentEntityService opponentEntityService;
-    // private final MoveEntityService moveEntityService;
+    private final BattleResultEntityService battleResultEntityService;
+    private final SpeciesEntityService speciesEntityService;
+    private final OpponentEntityService opponentEntityService;
+    private final MoveEntityService moveEntityService;
 
-    /*
-    create set of modified moves
-        save changes to db entity, or create it if it doesn't exist
-        remove battle results using old move
+    public EntityReconciliationService(
+        BattleResultEntityService battleResultEntityService,
+        SpeciesEntityService speciesEntityService,
+        OpponentEntityService opponentEntityService,
+        MoveEntityService moveEntityService
+    ) {
+        this.battleResultEntityService = battleResultEntityService;
+        this.speciesEntityService = speciesEntityService;
+        this.opponentEntityService = opponentEntityService;
+        this.moveEntityService = moveEntityService;
+    }
 
-    create set of modified species (species with moveset changes, who know a modified move, or that don't exist)
-        save changes to db entity, or create it if it doesn't exist
-        remove battle results using old species
+    public void start() {
+        /*
+        create set of modified moves
+            save changes to db entity, or create it if it doesn't exist
+            remove battle results using old move
 
-    create set modified opponents (opponents who use a species that has been modified)
-        save changes to db entity, or create it if it doesn't exist
-        remove battle results against old opponent
+        create set of modified species (species with moveset changes, who know a modified move, or that don't exist)
+            save changes to db entity, or create it if it doesn't exist
+            remove battle results using old species
 
-    rerun simulations for modified opponents
-        save simulation battle results
+        create set modified opponents (opponents who use a species that has been modified)
+            save changes to db entity, or create it if it doesn't exist
+            remove battle results against old opponent
 
-    rerun simulations for modified species (excluding those against modified opponents as they've already happened)
-        save simulation battle results
+        rerun simulations for modified opponents
+            save simulation battle results
 
-    */
+        rerun simulations for modified species (excluding those against modified opponents as they've already happened)
+            save simulation battle results
+
+        */
+    }
 }
